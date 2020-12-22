e2a5fc83007dfb5611d0d66ccf014af801c31b64
confusedSerge <serge.kotchourko@gmail.com>
2020-12-22 12:16:18 +0100

    counter and test with min

--- src/counter.py
+++ src/counter.py
@@ -7,9 +7,10 @@ from scipy.stats import spearmanr
 
 
 def word_freq_counter(targets, freq):
+    print(targets)
+    print(freq)
     freq_c = [0, 0, 0, 0]
     for (word1, word2) in targets:
-        print(freq_c)
         i = give_index(freq[word1])
         freq_c[i] += 1
         i = give_index(freq[word2])
